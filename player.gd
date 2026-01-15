53b081abb276d4b80a1ea7f7b26dd228c8a31b31
Dripcoding <dripcoding@gmail.com>
2026-01-14 22:25:01 -0700

    trigger player animations

--- player.gd
+++ player.gd
@@ -8,3 +8,9 @@ func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("Left", "Right", "Up", 'Down')
 	velocity = direction * speed
 	move_and_slide()
+	
+	# trigger animations depending on velocity vector len
+	if velocity.length() > 0:
+		$HappyBoo.play_walk_animation()
+	else:
+		$HappyBoo.play_idle_animation()
