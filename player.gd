03a94c686b837b64e53df8bdf86e8cdb5105d1b2
Dripcoding <dripcoding@gmail.com>
2026-01-14 22:10:20 -0700

    impl player movement

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @export var speed: float = 300.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("Left", "Right", "Up", 'Down')
-	velocity = direction * speed * delta
+	velocity = direction * speed
 	move_and_slide()
