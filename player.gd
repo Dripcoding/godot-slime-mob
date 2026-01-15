b5b5fea61e8c29c19a5941dcaa8d174c210ab9a6
Dripcoding <dripcoding@gmail.com>
2026-01-14 22:07:39 -0700

    create input map for player sprite

---
+++ player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+
+@export var speed: float = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("Left", "Right", "Up", 'Down')
+	velocity = direction * speed * delta
+	move_and_slide()
